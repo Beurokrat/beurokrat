c306043e393f1fdc180c93ab6e722ae5b02a0ba1
beurokrat <beurokrat@gmail.com>
2024-09-21 00:41:34 +0530

    hero update

--- src/app/home/company.tsx
+++ src/app/home/company.tsx
@@ -1,33 +1,35 @@
-"use client"
-import React from "react";
-import Button from "../_components/button";
+'use client'
+import React from 'react'
+import Button from '../_components/button'
 import { useRouter } from 'next/navigation'
-import Container from "../_components/container";
+import Container from '../_components/container'
 
 const AboutCompany = () => {
-  const router = useRouter()
-  const handleClick = () => {
-      router.push('/company')
+    const router = useRouter()
+    const handleClick = () => {
+        router.push('/company')
     }
-  return (
-    <section className=" bg-white h-[600px]">
-      <Container>
-      <div className="w-full flex flex-col items-center justify-start py-[100px] pr-5 pl-[21px] box-border gap-[50px_0px]  mq450:gap-[100px_0px] mq900:gap-[100px_0px]">
-        <footer className="pt-28 flex flex-col items-center justify-start gap-[30px_0px] max-w-full text-center text-smi text-bk-black font-cta-button-text-title-case mq450:gap-[50px_0px]">
-          <div className="flex flex-row items-start justify-start">
-            <h3>
-              START HERE, BUILD ANYTHING.
-            </h3>
-          </div>
-          <h1 className="m-0 md:w-3/4">
-            At Beurokrat, our products are our mode of expression
-          </h1>
-        </footer>
-        <Button text= "About Our Company" onClick={handleClick} />
-      </div>
-      </Container>
-    </section>
-  );
-};
+    return (
+        <section className=" bg-white h-[600px]">
+            <Container>
+                <div className="w-full flex flex-col items-center justify-start py-[100px] pr-5 pl-[21px] box-border gap-[50px_0px]  mq450:gap-[100px_0px] mq900:gap-[100px_0px]">
+                    <footer className="pt-28 flex flex-col items-center justify-start gap-[30px_0px] max-w-full text-center text-smi text-bk-black font-cta-button-text-title-case mq450:gap-[50px_0px]">
+                        <div className="flex flex-row items-start justify-start">
+                            <h3>START HERE, BUILD ANYTHING.</h3>
+                        </div>
+                        <h1 className="m-0 md:w-3/4">At Beurokrat, our products are our mode of expression</h1>
+                        <p className="m-0 text-md md:w-3/4">
+                            Engage with our community of experts pushing the limits of innovation and disrupting norms. We
+                            undertake AI, Web3, and Robotics projects. Through our collaborative efforts, we're not just building
+                            technologies – we're shaping the future of human-machine interaction, decentralized governance, and
+                            intelligent automation.
+                        </p>
+                    </footer>
+                    <Button text="About Our Company" onClick={handleClick} />
+                </div>
+            </Container>
+        </section>
+    )
+}
 
-export default AboutCompany;
+export default AboutCompany
--- src/app/home/hero/index.tsx
+++ src/app/home/hero/index.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef } from 'react'
 import Container from '@/app/_components/container'
 import HALO from 'vanta/dist/vanta.halo.min'
 import * as THREE from 'three'
+import splitStringUsingRegex from '@/helpers/regex-string-split'
 
 type VantaEffect = any
 
@@ -19,13 +20,19 @@ interface HeroProps {
 const Hero: React.FC<HeroProps> = (props) => {
     const [vantaEffect, setVantaEffect] = useState<VantaEffect | null>(null)
     const myRef = useRef<HTMLDivElement>(null)
-
+    splitStringUsingRegex
     useEffect(() => {
         if (!vantaEffect) {
             setVantaEffect(
                 HALO({
                     THREE,
                     el: myRef.current,
+                    backgroundColor: 0x000000, // Background color
+                    color: 0xdddddd, // Halo color
+                    size: 3.15, // Size of the halo effect
+                    amplitudeFactor: 1.0, // Amplitude of the waves
+                    baseFrequency: 0.9, // Frequency of the waves
+                    speed: 1.0, // Speed of the animation
                 })
             )
         }
@@ -33,15 +40,18 @@ const Hero: React.FC<HeroProps> = (props) => {
             if (vantaEffect) vantaEffect.destroy()
         }
     }, [vantaEffect])
-
+    const heading = 'Shaping the future of technology 🚀 and challenging the status quo.'
+    const text =
+        'Engage with our community of experts pushing the limits of innovation and disrupting norms. We undertake AI, Web3 and Robotics projects.'
     return (
         <div style={{ zIndex: -10 }} className="-full h-screen" ref={myRef}>
             <div className="w-full h-full flex items-center bg-black/30">
                 <Container>
-                    <div className="flex ">
-                        <h1 className="sm:text-4xl md:text-5xl bg-primary p-4 md:w-[40%] -mr-10 leading-10">
-                            Unleashing Innovation, One Byte at a Time.
-                        </h1>
+                    <div className="flex justify-center">
+                        <div className=" rounded-xl p-6 md:w-[60%]  text-gray-100">
+                            <h1 className="p-5 sm:text-5xl md:text-6xl leading-loose">{heading}</h1>
+                            <h1 className="p-5 sm:hidden md:text-md leading-loose">{text}</h1>
+                        </div>
                     </div>
                 </Container>
             </div>
