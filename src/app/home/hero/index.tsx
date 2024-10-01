788b1381a32ea559d4f47c52853a09d2f8d14518
beurokrat <beurokrat@gmail.com>
2024-10-01 16:27:13 +0530

    added text animation

--- src/app/home/hero/index.tsx
+++ src/app/home/hero/index.tsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect, useRef } from 'react'
 import Container from '@/app/_components/container'
 import HALO from 'vanta/dist/vanta.halo.min'
 import * as THREE from 'three'
+import splitStringUsingRegex from '@/helpers/split-string-regex'
+import { motion } from 'framer-motion'
 
 type VantaEffect = any
 
@@ -18,7 +20,28 @@ interface HeroProps {
 
 const Hero: React.FC<HeroProps> = (props) => {
     const [vantaEffect, setVantaEffect] = useState<VantaEffect | null>(null)
+    const heading = 'Shaping the future of technology and challenging the status quo.'
+    const text =
+        'Engage with our community of experts pushing the limits of innovation and disrupting norms. We undertake AI, Web3 and Robotics projects.'
+    const headingChars = splitStringUsingRegex(heading)
+    const textChars = splitStringUsingRegex(text)
     const myRef = useRef<HTMLDivElement>(null)
+
+    const containerVariants = {
+        hidden: { opacity: 1 },
+        visible: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.3, // Stagger each child by 0.3 seconds
+            },
+        },
+    }
+
+    const itemVariants = {
+        hidden: { opacity: 0, y: 20 },
+        visible: { opacity: 1, y: 0 },
+    }
+
     useEffect(() => {
         if (!vantaEffect) {
             setVantaEffect(
@@ -39,17 +62,41 @@ const Hero: React.FC<HeroProps> = (props) => {
             if (vantaEffect) vantaEffect.destroy()
         }
     }, [vantaEffect])
-    const heading = 'Shaping the future of technology 🚀 and challenging the status quo.'
-    const text =
-        'Engage with our community of experts pushing the limits of innovation and disrupting norms. We undertake AI, Web3 and Robotics projects.'
+
     return (
-        <div style={{ zIndex: -10 }} className="-full h-screen" ref={myRef}>
+        <div style={{ zIndex: -10 }} className="w-full h-screen" ref={myRef}>
             <div className="w-full h-full flex items-center bg-black/30">
                 <Container>
-                    <div className="flex justify-center">
-                        <div className=" rounded-xl p-6 md:w-[60%]  text-gray-100">
-                            <h1 className="p-5 sm:text-4xl md:text-6xl leading-loose">{heading}</h1>
-                            <h1 className="p-5  md:block sm:text-lg ">{text}</h1>
+                    <div className="flex">
+                        <div className="rounded-xl md:p-6 md:w-[60%] text-left text-gray-100">
+                            <motion.h1
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                transition={{
+                                    duration: 1,
+                                    delay: 0.7,
+                                }}
+                                className="p-1 sm:text-lg md:text-6xl"
+                            >
+                                {headingChars}
+                            </motion.h1>
+
+                            {/* {headingChars.map((el, i) => (
+                                <motion.span
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    transition={{
+                                        duration: 0.25,
+                                        delay: i / 20,
+                                    }}
+                                    key={i}
+                                    // variants={itemVariants}
+                                    className="p-1 sm:text-lg md:text-6xl text-center leading-loose"
+                                >
+                                    {el}
+                                </motion.span>
+                            ))} */}
+                            <motion.h1 className="p-5 sm:hidden sm:text-lg ">{textChars}</motion.h1>
                         </div>
                     </div>
                 </Container>
